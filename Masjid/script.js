77c1bcc7c8e71840bf6a07dea2c75f82e2c7ce83
mohamadaldi <mohamadaldi42@gmail.com>
2025-06-15 15:36:15 +0700

    Add files via upload

---
+++ Masjid/script.js
@@ -0,0 +1,7 @@
+// script.js
+// Tambahkan logika interaktif di sini jika diperlukan
+
+// Contoh interaksi sederhana: scroll ke atas saat halaman dimuat
+window.addEventListener('load', () => {
+  window.scrollTo(0, 0);
+});
