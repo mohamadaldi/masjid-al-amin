16593b1e898971382f935ee1c9f913f063a1e6f9
mohamadaldi <mohamadaldi42@gmail.com>
2025-08-21 13:20:28 +0700

    Delete Masjid directory

--- Masjid/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-// Tambahkan logika interaktif di sini jika diperlukan
-
-// Contoh interaksi sederhana: scroll ke atas saat halaman dimuat
-window.addEventListener('load', () => {
-  window.scrollTo(0, 0);
-});
